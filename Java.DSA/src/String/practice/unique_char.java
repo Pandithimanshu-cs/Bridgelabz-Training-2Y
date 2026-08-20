1e3d4d626278a756424fc858b14ab40ffbeddf32
Pandithimanshu-cs <dixitsatyaprakash26@gmail.com>
2026-08-20 23:10:23 +0530

    logic building

---
+++ Java.DSA/src/String/practice/unique_char.java
@@ -0,0 +1,22 @@
+package String.practice;
+
+import java.util.Scanner;
+
+public class unique_char {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int[] freq = new int[256];
+        for(int i =0;i<str.length();i++){
+            freq[str.charAt(i)]++;
+        }
+        for(int i =0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(freq[ch] == 1){
+                System.out.println(ch);
+                return;
+            }
+        }
+        System.out.println("No unique character");
+    }
+}
